eea78f1da4fb46985c7d0c0f47aa70fb745fb00a
Suhas Bhairav <suhasbhairav@gmail.com>
2020-11-30 00:10:53 +0100

    Learn react in a short time

---
+++ reactapp/src/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Todo({todo, toggleTodo}) {
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+    }
+
+
+    return (
+        <div>
+            <label>
+                <input type="checkbox" onChange={handleTodoClick} checked={todo.complete}/>
+                {todo.name}
+            </label>
+           {todo.name} 
+        </div>
+    )
+}
